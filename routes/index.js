66ad9caefd706160c3f65b0128fb559c00d0c46b
Ruairí <ruairitobrien@gmail.com>
2013-10-20 15:08:09 +0100

    Added temporary Openshift environment variable checks.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ exports.index = function (req, res) {
     var db = require('../apps/database/database');
     var util = require('util');
 
+
+    var t1 = typeof OPENSHIFT_APP_DNS;
+    var t2 = typeof OPENSHIFT_APP_NAME;
+    var t3 = typeof OPENSHIFT_APP_UUID;
+
     res.render('index',
         {
             title: 'FooForms',
@@ -15,7 +20,10 @@ exports.index = function (req, res) {
             arch: process.arch,
             platform: process.platform,
             nodeVersion: process.version,
-            isOpenShift: (typeof OPENSHIFT_APP_NAME !== 'undefined')
+            isOpenShift: (typeof OPENSHIFT_APP_NAME !== 'undefined'),
+            test1: t1,
+            test2: t2,
+            test3: t3
         }
     );
 };
