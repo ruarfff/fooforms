06d571dea7c4909182e75dbbd6713ee28c64432c
Ruairí <ruairitobrien@gmail.com>
2013-12-15 20:52:59 +0000

    Removed commented out code in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,67 +6,6 @@ var User = require('./apps/user/models/user').User;
 
 module.exports = function (grunt) {
 
-
-    /**
-
-
-     grunt.loadNpmTasks('grunt-nodemon');
-
-     grunt.registerTask('dbseed', 'seed the database', function () {
-        grunt.task.run('adduser:admin:admin@example.com:secret:true');
-     grunt.task.run('adduser:bob:bob@example.com:secret:false');
-     });
-
-     grunt.registerTask('adduser', 'add a user to the database', function (usr, emailaddress, pass, adm) {
-        // convert adm string to bool
-        adm = (adm === "true");
-
-        var user = new User(
-            {
-                username: usr,
-                email: emailaddress,
-                password: pass,
-                admin: adm
-            }
-        );
-
-        // save call is async, put grunt into async mode to work
-        var done = this.async();
-
-        user.save(function (err) {
-            if (err) {
-                console.log('Error: ' + err);
-                done(false);
-            } else {
-                console.log('saved user: ' + user.username);
-                done();
-            }
-        });
-    });
-
-     grunt.registerTask('dbdrop', 'drop the database', function () {
-        // async mode
-        var done = this.async();
-
-        database.connection.on('open', function () {
-            database.connection.db.dropDatabase(function (err) {
-                if (err) {
-                    console.log('Error: ' + err);
-                    done(false);
-                } else {
-                    console.log('Successfully dropped db');
-                    done();
-                }
-            });
-        });
-    });
-     grunt.registerTask('start', ['concurrent', function(){
-        grunt.task.run('dbseed');
-    }]);
-
-     **/
-
-
     require('load-grunt-tasks')(grunt);
 
 
