fe8eff92462808371e537473841808afacecc126
ruairi <ruairitobrien@gmail.com>
2014-01-15 20:17:13 +0000

    Updated profile route to import bunch of scripts it probably doesn't need. Just experimenting for now,

--- apps/user/routes.js
+++ apps/user/routes.js
@@ -1,46 +1,51 @@
 /*jslint node: true */
 'use strict';
 
-var config = require( '../../config/config' );
+var config = require('../../config/config');
 var viewDir = config.root + '/apps/user/views';
-var authentication = require( '../authentication/lib' );
-var userApi = require( './api' );
-var path = require( 'path' );
+var authentication = require('../authentication/lib');
+var userApi = require('./api');
+var path = require('path');
 
-var routes = function ( app ) {
+var routes = function (app) {
 
     /*********************************************************************************
      *  View Handlers
      *********************************************************************************/
 
-    app.get( '/profile', authentication.ensureAuthenticated, function ( req, res ) {
+    app.get('/profile', authentication.ensureAuthenticated, function (req, res) {
         var user = req.user;
 
-        res.render( path.join( viewDir, 'profile' ), {
+        res.render(path.join(viewDir, 'profile'), {
             user: user,
             title: 'Profile',
-            scripts: ['javascripts/user/controllers/controllers.js']
-        } );
+            scripts: [
+                'javascripts/user/controllers/profileController.js',
+                'javascripts/user/directives/profileDirective.js',
+                'javascripts/user/filters/profileFilter.js',
+                'javascripts/user/services/profileService.js'
+            ]
+        });
 
-    } );
+    });
 
-    app.get( '/people', authentication.ensureAuthenticated, function ( req, res ) {
+    app.get('/people', authentication.ensureAuthenticated, function (req, res) {
         var user = req.user;
 
-        res.render( path.join( viewDir, 'people' ), {
+        res.render(path.join(viewDir, 'people'), {
             user: user,
             title: 'People'
-        } );
+        });
 
-    } );
+    });
 
     /*********************************************************************************
      *  API
      *********************************************************************************/
 
-    app.get( '/api/user/me', authentication.ensureAuthenticated, function ( req, res ) {
-        userApi.me( req, res );
-    } );
+    app.get('/api/user/me', authentication.ensureAuthenticated, function (req, res) {
+        userApi.me(req, res);
+    });
 
 };
 
--- public/javascripts/user/controllers/profileController.js
+++ public/javascripts/user/controllers/profileController.js
@@ -1,8 +1,8 @@
 var userApp = angular.module('userApp', []);
 
-userApp.controller('ProfileCtrl', function ($scope, $http){
+userApp.controller('ProfileCtrl', function ($scope, $http) {
     'use strict';
-    $http.get('/api/user/me').success(function(data){
+    $http.get('/api/user/me').success(function (data) {
         $scope.profile = data;
         alert('hello');
     })
