3b2ab1d7b79b1d24237e8cc432c0d44e9efb2d1e
Brian <brian@fooforms.com>
2014-01-25 19:20:20 +0000

    App Builder

---
+++ public/javascripts/appBuilder/app/appBuilder.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mcaulb1 on 1/17/14.
+ */
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('appBuilder', [
+    'ngRoute',
+    'appBuilder.filters',
+    'appBuilder.services',
+    'appBuilder.directives',
+    'appBuilder.controllers',
+    'ui.bootstrap'
+])
