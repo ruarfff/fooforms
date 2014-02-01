c9cf66c252bfac2742f313156607f2cb3abcc397
ruairi <ruairitobrien@gmail.com>
2014-02-01 20:54:14 +0000

    Fixed logout

--- apps/authentication/routes.js
+++ apps/authentication/routes.js
@@ -1,13 +1,13 @@
 /*jslint node: true */
 'use strict';
 
-var path = require( 'path' );
-var authApi = require( path.join( global.config.apps.AUTHENTICATION, 'api/authenticationApi' ) );
-var authLib = require( global.config.apps.AUTHENTICATION );
-var log = require( global.config.apps.LOGGING ).LOG;
+var path = require('path');
+var authApi = require(path.join(global.config.apps.AUTHENTICATION, 'api/authenticationApi'));
+var authLib = require(global.config.apps.AUTHENTICATION);
+var log = require(global.config.apps.LOGGING).LOG;
 
 
-var routes = function ( app, passport ) {
+var routes = function (app, passport) {
 
     /*********************************************************************************
      *  View Handlers
@@ -16,21 +16,21 @@ var routes = function ( app, passport ) {
     /**
      * Show login form
      */
-    app.get( '/login', function ( req, res ) {
-        res.render( authLib.loginPath, {
+    app.get('/login', function (req, res) {
+        res.render(authLib.loginPath, {
             title: 'Login',
-            message: req.flash( 'error' )
-        } );
-    } );
+            message: req.flash('error')
+        });
+    });
 
     /**
      * Show sign up form
      */
-    app.get( '/signup', function ( req, res ) {
-        res.render( authLib.signupPath, {
+    app.get('/signup', function (req, res) {
+        res.render(authLib.signupPath, {
             title: 'Sign up'
-        } );
-    } );
+        });
+    });
 
     /*********************************************************************************
      *  Local Handlers
@@ -39,22 +39,21 @@ var routes = function ( app, passport ) {
     /**
      * Logout
      */
-    app.get( '/logout', function ( req, res ) {
-        log.debug( res.user.displayName + ' logging out' );
+    app.get('/logout', function (req, res) {
         req.logout();
-        res.redirect( '/' );
-    } );
+        res.redirect('/');
+    });
 
-    app.post( '/login',
-        passport.authenticate( 'local', { successRedirect: '/dashboard',
+    app.post('/login',
+        passport.authenticate('local', { successRedirect: '/dashboard',
             failureRedirect: '/login',
-            failureFlash: true } )
+            failureFlash: true })
     );
 
 
-    app.post( '/signup', function ( req, res ) {
-        authApi.signup( req, res );
-    } );
+    app.post('/signup', function (req, res) {
+        authApi.signup(req, res);
+    });
 
     /*********************************************************************************
      *  Provider Handlers
@@ -62,69 +61,69 @@ var routes = function ( app, passport ) {
     var successRedirect = '/dashboard';
     var failureRedirect = '/login';
 
-    app.get( '/auth/google',
-        passport.authenticate( 'google', {
+    app.get('/auth/google',
+        passport.authenticate('google', {
             scope: ['https://www.googleapis.com/auth/userinfo.profile',
                 'https://www.googleapis.com/auth/userinfo.email']
-        } ),
+        }),
         function () {
             // The request will be redirected to Google for authenticator, so this
             // function will not be called.
-        } );
+        });
 
-    app.get( '/auth/google/return',
-        passport.authenticate( 'google', { failureRedirect: failureRedirect } ),
-        function ( req, res ) {
-            res.redirect( successRedirect );
-        } );
+    app.get('/auth/google/return',
+        passport.authenticate('google', { failureRedirect: failureRedirect }),
+        function (req, res) {
+            res.redirect(successRedirect);
+        });
 
 
-    app.get( '/auth/twitter', passport.authenticate( 'twitter' ) );
+    app.get('/auth/twitter', passport.authenticate('twitter'));
 
     //Twitter will redirect the user to this URL after approval.  Finish the
     //authenticator process by attempting to obtain an access token.  If
     //access was granted, the user will be logged in.  Otherwise,
     //authenticator has failed.
-    app.get( '/auth/twitter/callback',
-        passport.authenticate( 'twitter', {
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
             successRedirect: successRedirect,
             failureRedirect: failureRedirect
-        } ) );
+        }));
 
-    app.get( '/auth/facebook',
-        passport.authenticate( 'facebook', { scope: ['user_status', 'email'] } )
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', { scope: ['user_status', 'email'] })
     );
 
-    app.get( '/auth/facebook/callback',
-        passport.authenticate( 'facebook', {
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
             successRedirect: successRedirect,
             failureRedirect: failureRedirect
-        } ) );
+        }));
 
-    app.get( '/auth/yahoo',
-        passport.authenticate( 'yahoo', { failureRedirect: failureRedirect } ),
-        function ( req, res ) {
-            res.redirect( successRedirect );
-        } );
+    app.get('/auth/yahoo',
+        passport.authenticate('yahoo', { failureRedirect: failureRedirect }),
+        function (req, res) {
+            res.redirect(successRedirect);
+        });
 
-    app.get( '/auth/yahoo/return',
-        passport.authenticate( 'yahoo', { failureRedirect: failureRedirect } ),
-        function ( req, res ) {
-            res.redirect( successRedirect );
-        } );
+    app.get('/auth/yahoo/return',
+        passport.authenticate('yahoo', { failureRedirect: failureRedirect }),
+        function (req, res) {
+            res.redirect(successRedirect);
+        });
 
-    app.get( '/auth/linkedin',
-        passport.authenticate( 'linkedin' ),
+    app.get('/auth/linkedin',
+        passport.authenticate('linkedin'),
         function () {
             // The request will be redirected to LinkedIn for authenticator, so this
             // function will not be called.
-        } );
+        });
 
-    app.get( '/auth/linkedin/callback',
-        passport.authenticate( 'linkedin', { failureRedirect: failureRedirect } ),
-        function ( req, res ) {
-            res.redirect( successRedirect );
-        } );
+    app.get('/auth/linkedin/callback',
+        passport.authenticate('linkedin', { failureRedirect: failureRedirect }),
+        function (req, res) {
+            res.redirect(successRedirect);
+        });
 
 };
 
