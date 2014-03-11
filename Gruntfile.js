860eb1bda8aea85149da2dd6429332fb7110702f
Ruairi O Brien <ruairitobrien@gmail.com>
2014-03-11 22:50:33 +0000

    Updated test coverage in Grunffile

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,13 +32,12 @@ module.exports = function (grunt) {
             coverage: {
                 options: {
                     reporter: 'html-cov',
-                    // use the quiet flag to suppress the mocha console output
                     quiet: true,
                     // specify a destination file to capture the mocha
                     // output (the quiet option does not suppress this)
                     captureFile: 'frontend/public/coverage.html'
                 },
-                src: ['test/**/*.js']
+                src: ['test/spec/**/*.js']
             }
         },
         concurrent: {
