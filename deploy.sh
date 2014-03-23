e2a6e638896b624a814b31f7860e36f699bb6be4
Ruairi O Brien <ruairitobrien@gmail.com>
2014-03-23 23:57:40 +0000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Running Application in Forever"
-NODE_ENV=production forever start server.js
+NODE_ENV=production forever stopall -v server.js
+NODE_ENV=production forever start -v server.js
