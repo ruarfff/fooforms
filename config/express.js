00b2356e947bc95bc9848c819c0a2a1bef24a2bc
Ruairi O Brien <ruairitobrien@gmail.com>
2014-04-06 13:44:04 +0100

    Fixed issue with favicon
    https://github.com/visionmedia/express/issues/1979

--- config/express.js
+++ config/express.js
@@ -32,7 +32,7 @@ module.exports = function (app, passport) {
         app.set('files', global.config.root + '/files');
         app.engine('.html', engine.__express);
         app.set('view engine', 'html');
-        app.use(express.favicon());
+        app.use(express.favicon(global.config.root + '/frontend/public/assets/ico/favicon.ico'));
         app.use(express.static(global.config.root + '/frontend/public'));
         app.use(express.json());
         app.use(express.urlencoded());
