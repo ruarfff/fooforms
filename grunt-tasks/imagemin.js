fd73d176704ac0479533aca5e8f73ee5bd0829d5
Ruairi O Brien <ruairitobrien@gmail.com>
2014-10-05 17:57:51 +0100

    Added image min

---
+++ grunt-tasks/imagemin.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.config('imagemin', {
+
+        png: {
+            options: {
+                optimizationLevel: 7
+            },
+            files: [
+                {
+                    // Set to true to enable the following options…
+                    expand: true,
+                    // cwd is 'current working directory'
+                    cwd: 'frontend/public/assets/images/',
+                    src: ['**/*.png'],
+                    // Could also match cwd line above. i.e. project-directory/img/
+                    dest: 'frontend/public/assets/images/compressed/',
+                    ext: '.png'
+                }
+            ]
+        },
+        jpg: {
+            options: {
+                progressive: true
+            },
+            files: [
+                {
+                    // Set to true to enable the following options…
+                    expand: true,
+                    // cwd is 'current working directory'
+                    cwd: 'frontend/public/assets/images/',
+                    src: ['**/*.jpg'],
+                    // Could also match cwd. i.e. project-directory/img/
+                    dest: 'frontend/public/assets/images/compressed/',
+                    ext: '.jpg'
+                }
+            ]
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+};
