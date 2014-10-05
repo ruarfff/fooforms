02b980aac34a2f31d8a08f8d15bca72e957af737
Ruairi O Brien <ruairitobrien@gmail.com>
2014-10-05 14:32:00 +0100

    Removing coverage to see what happens

--- grunt-tasks/mochaTest.js
+++ grunt-tasks/mochaTest.js
@@ -10,16 +10,6 @@ module.exports = function(grunt) {
                 clearRequireCache: true
             },
             src: ['modules/**/test/*-spec.js']
-        },
-        coverage: {
-            options: {
-                reporter: 'html-cov',
-                quiet: true,
-                //specify a destination file to capture the mocha
-                //output (the quiet option does not suppress this)
-                captureFile: 'frontend/public/coverage.html'
-            },
-            src: ['modules/**/test/*-spec.js']
         }
     });
     grunt.loadNpmTasks('grunt-mocha-test');
