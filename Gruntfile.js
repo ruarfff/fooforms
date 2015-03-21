08e93ec1b9c6466278a69f07de222b529b5aab5d
Ruairi O Brien <ruairitobrien@gmail.com>
2015-03-21 00:24:17 +0000

    Update grunt test task to set node-env to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
     grunt.registerTask('deploy', 'deploy pre-processed assets', ['bower', 'newer:concat', 'newer:uglify', 'sass', 'newer:imagemin']);
     grunt.registerTask('preprocessing', 'deploy pre-processed assets, customised a little for nodejitsu', ['bower', 'concat', 'uglify', 'sass']);
     grunt.registerTask('default', 'start application in dev mode running tests and using watch', ['deploy', 'mochaTest', 'express:dev', 'watch']);
-    grunt.registerTask('test', 'only run tests and generate coverage report', ['mochaTest']);
+    grunt.registerTask('test', 'only run tests and generate coverage report', ['env:test', 'mochaTest']);
     grunt.registerTask('skip-test', 'start application in dev mode using watch and nodemon', ['deploy', 'express:dev', 'watch']);
     grunt.registerTask('brian', 'start app in dev mode only running UI tests', ['deploy']);
 
